4b501c71ea8f49860eb1e71741a274032e000243
Yash Sanjivkumar Pawar <rekhaspawar7@gmail.com>
2026-07-16 16:54:06 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,9 +27,6 @@ module.exports = {
       },
       fontFamily: {
         sans: ["Inter", "system-ui", "sans-serif"],
-        display: ["Manrope", "system-ui", "sans-serif"],
-        mono: ['"JetBrains Mono"', "ui-monospace", "SFMono-Regular", "monospace"],
-        quote: ['"IBM Plex Sans"', "Georgia", "serif"],
       },
     },
   },
